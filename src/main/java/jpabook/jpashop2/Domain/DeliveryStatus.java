19fc148285167f624818f00d9177aa404696676b
q1q1w2w2 <106463879+q1q1w2w2@users.noreply.github.com>
2024-07-12 20:55:05 +0900

    Delete src directory

--- src/main/java/jpabook/jpashop2/Domain/DeliveryStatus.java
+++
@@ -1,5 +0,0 @@
-package jpabook.jpashop2.Domain;
-
-public enum DeliveryStatus {
-    READY, COMP
-}
--- src/main/java/jpabook/jpashop2/Domain/OrderStatus.java
+++
@@ -1,5 +0,0 @@
-package jpabook.jpashop2.Domain;
-
-public enum OrderStatus {
-    ORDER, CANCEL
-}
