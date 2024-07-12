ee21df9c10b8b1feb896d09d44bc2fe2afb86176
q1q1w2w2 <106463879+q1q1w2w2@users.noreply.github.com>
2024-07-12 20:57:01 +0900

    2024.07.12
    컨트롤러에서 엔티티 그대로 사용하지 말고 폼을 만들어서 컨트롤러에서 정제해서 넘기기(스프링부트와JPA활용1-7-회원등록)
    요구사항이 단순할 때는 엔티티를 그대로 사용하지만, 일반적으로 복잡하므로 폼을 만들어서 유지보수하기 좋게 만들기
    
    준영속 엔티티 -> 영속성 컨텍스트가 더 이상 관리하지 않는 엔티티(DB에 한 번 저장되었어서, 식별자가 이미 있음)
    JPA가 관리 안함 -> 변경감지(dirty checking 발생 안함) 
    해결 방법1: 준영속 엔티티지만 변경감지를 사용
    영속상태로 꺼냄 -> 끝나면 커밋 -> flush발생 -> 변경감지 -> 업데이트
    해결 방법2: Merge 사용(병합)
    merge실행 -> 파라미터로 받은 준영속 엔티티의 식별자 값으로 1차 캐시에서 엔티티 조회 -> 없으면 DB에서 조회하고 1차캐시에 저장 -> 조회한 영속 엔티티에 준영속 엔티티 값을 채워 넣고 영속상태 엔티티를 반환
    *주의* merge사용시 모든 속성이 변경되므로 값이 없으면 null로 업데이트 함
    
    결론 -> merge로 깔끔하게 하기 어렵기 때문에 변경 감지 사용하기

--- jpashop2/src/main/java/jpabook/jpashop2/service/ItemService.java
+++ jpashop2/src/main/java/jpabook/jpashop2/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop2.service;
 
+import jpabook.jpashop2.Domain.Book;
 import jpabook.jpashop2.Domain.Item;
 import jpabook.jpashop2.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,16 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        //영속상태로 꺼냄 -> 끝나면 커밋 -> flush 발생 -> 변경감지 -> 업데이트
+        Item findItem = itemRepository.findOne(itemId);
+//        findItem.change(price, name, stockQuantity); // 이렇게 의미있는 메서드로 만들어야 함 setter말고
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
     }
 
     public List<Item> findItems() {
---
+++ jpashop2/src/main/java/jpabook/jpashop2/service/UpdateItemDto.java
@@ -0,0 +1,10 @@
+package jpabook.jpashop2.service;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class UpdateItemDto {
+
+
+}
